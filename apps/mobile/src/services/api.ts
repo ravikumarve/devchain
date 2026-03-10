7a31cd1ee9ad4ea05346ab9e687d3374e84606a0
ravikumarve <ravikumar.1690@yahoo.in>
2026-03-11 03:12:19 +0530

    feat: switch frontend to production API

--- apps/mobile/src/services/api.ts
+++ apps/mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://192.168.0.105:5000/api/v1';
+const API_URL = 'https://devchain.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
